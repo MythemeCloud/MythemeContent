13b8a17b977057cb29c121447d811914ebb7001e
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-10 15:24:22 +0200

    Add import syntax and config info

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,25 +1,20 @@
-import path from 'path';
-import { buildConfig } from 'payload/config';
-import Categories from './collections/Categories';
-import Posts from './collections/Posts';
-import Tags from './collections/Tags';
-import Users from './collections/Users';
+import path from "path";
+import { buildConfig } from "payload/config";
+import Categories from "./collections/Categories";
+import Posts from "./collections/Posts";
+import Tags from "./collections/Tags";
+import Users from "./collections/Users";
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
-  admin: {
-    user: Users.slug,
-  },
-  collections: [
-    Categories,
-    Posts,
-    Tags,
-    Users,
-  ],
-  typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts')
-  },
-  graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
-  },
+	serverURL: "http://localhost:3000",
+	admin: {
+		user: Users.slug,
+	},
+	collections: [Categories, Posts, Tags, Users],
+	typescript: {
+		outputFile: path.resolve(__dirname, "payload-types.ts"),
+	},
+	graphQL: {
+		schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+	},
 });
